f11cef45e9b84a405fdc76405a2e40eff5f99992
tcs <tcs@tcshemn-pc.(none)>
2014-03-24 16:39:43 +0530

    Minor modifications

--- drivers/netconf/netconf_fwaas.py
+++ drivers/netconf/netconf_fwaas.py
@@ -34,17 +34,17 @@ class NetconfFwaasDriver(fwaas_base.FwaasDriverBase):
         self.__uname = "user="+uname
         self.__password = "password="+password
         self.__ncport = "ncport="+ncport
-		self.root_helper = root_helper
-		self.__network_id = network_id
+	self.root_helper = root_helper
+	self.__network_id = network_id
 
     def create_firewall(self, apply_list, firewall):
-		LOG.debug(_('Creating firewall %(fw_id)s for tenant %(tid)s)'),
+	LOG.debug(_('Creating firewall %(fw_id)s for tenant %(tid)s)'),
                   {'fw_id': firewall['id'], 'tid': firewall['tenant_id']})
         
         return self.update_firewall(apply_list, firewall)
 
     def update_firewall(self, apply_list, firewall):
-		LOG.debug(_('Updating firewall %(fw_id)s for tenant %(tid)s)'),
+	LOG.debug(_('Updating firewall %(fw_id)s for tenant %(tid)s)'),
                   {'fw_id': firewall['id'], 'tid': firewall['tenant_id']})
         
         if firewall['admin_state_up']:
@@ -53,7 +53,7 @@ class NetconfFwaasDriver(fwaas_base.FwaasDriverBase):
             return self.apply_default_policy(apply_list, firewall)
 
     def delete_firewall(self, apply_list, firewall):
-		LOG.debug(_('Deleting firewall %(fw_id)s for tenant %(tid)s)'),
+	LOG.debug(_('Deleting firewall %(fw_id)s for tenant %(tid)s)'),
                   {'fw_id': firewall['id'], 'tid': firewall['tenant_id']})
         
         command = "delete /vyatta-firewall"
@@ -62,52 +62,52 @@ class NetconfFwaasDriver(fwaas_base.FwaasDriverBase):
         return True
 
     def apply_default_policy(self, apply_list, firewall):
-		LOG.debug(_('Applying firewall %(fw_id)s for tenant %(tid)s)'),
+	LOG.debug(_('Applying firewall %(fw_id)s for tenant %(tid)s)'),
                   {'fw_id': firewall['id'], 'tid': firewall['tenant_id']})
 
-		firewall_policy = firewall['firewall_policy_id']
+	firewall_policy = firewall['firewall_policy_id']
 		
-		#Delete firewall policy or rule? Apply default policy hardcoded?
-		# delete /vyatta-firewall/firewall-name[name='%s']/rule
-		# replace /vyatta-firewall/firewall-name[name='%s']/default-action --value='drop'
+	#Delete firewall policy or rule? Apply default policy hardcoded?
+	# delete /vyatta-firewall/firewall-name[name='%s']/rule
+	# replace /vyatta-firewall/firewall-name[name='%s']/default-action --value='drop'
 		
-		command_list = []
-		command_list.append("delete /vyatta-firewall/firewall-name[name='%s']/rule" % firewall_policy)
-		command_list.append("replace /vyatta-firewall/firewall-name[name='%s']/default-action --value='drop'" % firewall_policy)
+	command_list = []
+	command_list.append("delete /vyatta-firewall/firewall-name[name='%s']/rule" % firewall_policy)
+	command_list.append("replace /vyatta-firewall/firewall-name[name='%s']/default-action --value='drop'" % firewall_policy)
 		
-		for command in command_list:
-		    self.executeCommands(command)
+	for command in command_list:
+	    self.executeCommands(command)
 	    
-	    return True
+        return True
     
     def create_nat(self, apply_list, nat):
-		LOG.debug(_('Applying NAT %(nat_id)s for tenant %(tid)s)'),
-                  {'nat_id': nat['id'], 'tid': nat['tenant_id']})
+	LOG.debug(_('Applying NAT %(nat_id)s for tenant %(tid)s)'),
+                 {'nat_id': nat['id'], 'tid': nat['tenant_id']})
         
-	    command_list = []
+        command_list = []
 	    
-	    vyatta_rules = {
+	vyatta_rules = {
 			"source_address": "replace /vyatta-nat/source/rule[name='%s']/source/address --value='%s'",
 			"outbound_interface": "replace /vyatta-nat/source/rule[name='%s']/outbound-interface --value='%s'",
 			"destination_address": "replace /vyatta-nat/source/rule[name='%s']/destination/address --value='%s'",
 			"translation": "replace /vyatta-nat/source/rule[name='%s']/translation/address --value='%s'"	
         }
 	    
-	    command_list.append("replace /vyatta-nat/source/rule[name='%s']" % nat['id'])
+	command_list.append("replace /vyatta-nat/source/rule[name='%s']" % nat['id'])
 	    
-	    for k,v in vyatta_rules:
-			if nat[k]:
-				command_list.append(v % (nat['id'], nat[k]))
+	for k,v in vyatta_rules:
+	    if nat[k]:
+	        command_list.append(v % (nat['id'], nat[k]))
 		
 		LOG.debug(_("NAT Policy Command list (%s)"), command_list)
 		
 		for command in commandlist:
 		    self.executeCommands(command)
 	    
-	    return True
+	return True
 	
     def delete_nat(self, apply_list, nat):
-		LOG.debug(_('Deleting NAT %(nat_id)s for tenant %(tid)s)'),
+	LOG.debug(_('Deleting NAT %(nat_id)s for tenant %(tid)s)'),
                   {'nat_id': nat['id'], 'tid': nat['tenant_id']})
         
         command = "delete /vyatta-nat"
@@ -116,12 +116,12 @@ class NetconfFwaasDriver(fwaas_base.FwaasDriverBase):
         return True
     
     def create_zone(self, apply_list, zone):
-	    LOG.debug(_('Applying ZONE %(zone_name)s for tenant %(tid)s)'),
+        LOG.debug(_('Applying ZONE %(zone_name)s for tenant %(tid)s)'),
                   {'zone_name': zone['zone_name'], 'tid': zone['tenant_id']})
         
-	    command_list = []
+        command_list = []
 	    
-	    vyatta_rules = {
+        vyatta_rules = {
 			"interface": "replace /vyatta-zone-policy/zone[name='%s']/interface --value='%s'",
 			"default_action": "replace /vyatta-zone-policy/zone[name='%s']/default_action --value='%s'",
 			"from_zone": "replace /vyatta-zone-policy/zone[name='%s']/from[name='%s']",
@@ -131,18 +131,18 @@ class NetconfFwaasDriver(fwaas_base.FwaasDriverBase):
         command_list.append("replace /vyatta-zone-policy/zone[name='%s']" % zone['zone_name'])
         
         for k,v in vyatta_rules:
-			if zone[k]:
-				command_list.append(v % (zone['zone_name'], zone[k]))
+	    if zone[k]:
+		command_list.append(v % (zone['zone_name'], zone[k]))
 		
 		LOG.debug(_("ZONE Command list (%s)"), command_list)
 		
 		for command in command_list:
 		    self.executeCommands(command)
 	    
-	    return True
+        return True
     
     def delete_zone(self, apply_list, zone):
-		LOG.debug(_('Deleting ZONE %(zone_name)s for tenant %(tid)s)'),
+	LOG.debug(_('Deleting ZONE %(zone_name)s for tenant %(tid)s)'),
                   {'zone_name': zone['zone_name'], 'tid': nat['tenant_id']})
         
         command = "delete /vyatta-zone-policy"
@@ -150,7 +150,7 @@ class NetconfFwaasDriver(fwaas_base.FwaasDriverBase):
         
         return True
     
-	def _update_firewall(self, apply_list, firewall):
+    def _update_firewall(self, apply_list, firewall):
         LOG.debug(_("Updating firewall (%s)"), firewall['id'])
         
         firewall_policy = firewall['firewall_policy_id']
@@ -171,39 +171,39 @@ class NetconfFwaasDriver(fwaas_base.FwaasDriverBase):
         command_list.append("replace /vyatta-firewall /firewall-name[name='%s']/default-action --value='drop'" % firewall_policy_name)
         
         if firewall_policy['firewall_rules'] is not None:
-			for rule in firewallpolicy['firewall_rules']:
-				if not rule['enabled']:
-					continue
+	    for rule in firewallpolicy['firewall_rules']:
+		if not rule['enabled']:
+		    continue
 				
-				if rule['ip_version'] == 4:
-					for k,v in vyatta_rules:
-						if rule[k]:
-							command_list.append(v % (firewall_policy_name, rule['id'], rule[k]))
-				else:
-					LOG.warn(_("Unsupported IP version rule."))
+		if rule['ip_version'] == 4:
+		    for k,v in vyatta_rules:
+		        if rule[k]:
+			    command_list.append(v % (firewall_policy_name, rule['id'], rule[k]))
+			else:
+			    LOG.warn(_("Unsupported IP version rule."))
 		
 		LOG.debug(_("Firewall Policy Command list (%s)"), command_list)
 
-	    for command in command_list:
-		    self.executeCommands(command)
+        for command in command_list:
+	    self.executeCommands(command)
 	    
-	    return True
+        return True
 	
 	
 
-	def executeCommands(self, command):
-		executionList = ['/home/anirudh/workspace1/OpenYuma-master/netconf/target/bin/yangcli']
-		executionList.extend([self.__ip, self.__uname, self.__password, self.__ncport])
-		executionList.append(command);
+    def executeCommands(self, command):
+	executionList = ['/home/anirudh/workspace1/OpenYuma-master/netconf/target/bin/yangcli']
+	executionList.extend([self.__ip, self.__uname, self.__password, self.__ncport])
+	executionList.append(command);
 		
-		# TODO: How to get Namespace.. something fishy
-		# TODO: Is apply_list is list of Firewalls???
-		namespace = "qdhcp-" + self.__network_id
-		ns = ip_lib.IPWrapper(self.root_helper, namespace)
+	# TODO: How to get Namespace.. something fishy
+	# TODO: Is apply_list is list of Firewalls???
+	namespace = "qdhcp-" + self.__network_id
+	ns = ip_lib.IPWrapper(self.root_helper, namespace)
+	
+	while(True):
+	    retVal = ns.netns.execute(executionList,addl_env)
+	    if  retVal.find("mgr_io failed") == -1:
+		break;
 		
-		while(True):
-			retVal = ns.netns.execute(executionList,addl_env)
-			if  retVal.find("mgr_io failed") == -1:
-				break;
-		
-		return retVal
+	return retVal
